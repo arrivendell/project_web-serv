13d6f187b789d3d7a46f248e59c167cb1ad6dd0e
golgoth <adrien.duffy@gmail.com>
2016-02-24 17:16:18 +0000

    commit cause of ubuntu bug

--- config.py
+++ config.py
@@ -64,7 +64,6 @@ class GlobalConfig:
         """ Construct with the development configuration """
 
         self.mongo_db = GlobalConfig.MongoDB()
-        self.database_log = True
         self.web_server = GlobalConfig.WebServer()
         self.path_log = "./log/"
         self.logger_level = logging.DEBUG
@@ -76,7 +75,6 @@ class GlobalConfig:
             dict_config = json.loads(infile.read())
         #global_config = cls()
         self.mongo_db = GlobalConfig.MongoDB.from_config(dict_config['mongo_db'])
-        self.database_log = dict_config['database_log']
         self.web_server = GlobalConfig.WebServer.from_config(dict_config['web_server'])
         self.path_log = dict_config['path_log']
         self.logger_level = dict_config['logger_level']
@@ -88,7 +86,6 @@ class GlobalConfig:
         dict_config['mongo_db'] = self.mongo_db.save_config()    
         dict_config['web_server'] = self.web_server.save_config()          
         dict_config['path_log'] = self.path_log
-        dict_config['str_to_replace_id'] = self.str_to_replace_id
         dict_config['logger_level'] = self.logger_level
         with open(self.path_config, 'w') as outfile:
             outfile.write(json.dumps(dict_config))
